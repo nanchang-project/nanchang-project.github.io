9b697a8a2b6645160d17b94d92a9159dbead2ead
Kadin Campbell <kadincampbell@Kadins-MacBook-Pro.local>
2024-04-09 19:27:21 -0400

    Change suggestions

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 window.onload = () => {
     let howToDownloadRevealButtons = document.getElementsByClassName("btn-how-to-download");
     for (let i = 0; i < howToDownloadRevealButtons.length; i++) {
-        howToDownloadRevealButtons[i].addEventListener("click", revealClickedDownloadMethod)
+        howToDownloadRevealButtons[i].addEventListener("click", revealClickedDownloadMethod);
     }
+
+    let displayScriptButton = document.getElementById("display-script-button");
+    displayScriptButton.addEventListener("click", revealScriptOnClick);
 };
 
 function revealClickedDownloadMethod(event) {
@@ -17,4 +20,34 @@ function revealClickedDownloadMethod(event) {
     let methodToReveal = event.currentTarget.id;
     let elementToReveal = document.querySelector(`.${methodToReveal}`)
     elementToReveal.classList.add("reveal-method");
+}
+
+function revealScriptOnClick() {
+    const script = document.getElementById("script");
+    script.classList.add("reveal");
+
+    let scriptText = "";
+    for (let i = 0; i < script.childNodes.length; i++) {
+        console.log(script.childNodes[i]);
+
+        if (script.childNodes[i].nodeName == "BUTTON") {
+            continue;
+        }
+        else if (script.childNodes[i].innerText) {
+            scriptText += script.childNodes[i].innerText;
+
+        } else {
+            scriptText += script.childNodes[i].nodeValue + '\n';
+            
+        }
+    }
+
+    console.log(scriptText);
+
+    let copyScriptToClipboard = document.querySelector(".script-copy-button");
+    copyScriptToClipboard.addEventListener("click", copyToClipboard(scriptText));
+}
+
+function copyToClipboard(scriptText) {
+    navigator.clipboard.writeText(scriptText);
 }
